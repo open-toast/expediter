3fabc75544d608daf20c5c64b99bc3bc19976ecb
Oleg Golberg <ogolberg@toasttab.com>
2024-02-25 20:26:05 -0500

    Clean up duplicated root selector code

--- plugin/src/main/kotlin/com/toasttab/expediter/gradle/config/RootSelectorSpec.kt
+++ plugin/src/main/kotlin/com/toasttab/expediter/gradle/config/RootSelectorSpec.kt
@@ -1,8 +1,21 @@
+/*
+ * Copyright (c) 2024 Toast Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.toasttab.expediter.gradle.config
 
 import com.toasttab.expediter.roots.RootSelector
-import com.toasttab.expediter.types.ApplicationType
-import com.toasttab.expediter.types.ClassfileSourceType
 
 open class RootSelectorSpec {
     var type: RootType = RootType.PROJECT_CLASSES
@@ -18,7 +31,5 @@ open class RootSelectorSpec {
 
 enum class RootType(val selector: RootSelector) {
     ALL(RootSelector.All),
-    PROJECT_CLASSES(object : RootSelector {
-        override fun isRoot(type: ApplicationType) = type.source.type == ClassfileSourceType.SOURCE_SET || type.source.type == ClassfileSourceType.SUBPROJECT_DEPENDENCY
-    })
+    PROJECT_CLASSES(RootSelector.ProjectClasses)
 }
