493bb1625fa3d6f693844f28b5133b6d8d432ecf
Oleg Golberg <ogolberg@toasttab.com>
2026-01-26 17:32:59 -0500

    Test against AGP9 (#224)
    Making sure we test against both AGP8 and AGP9.

---
+++ plugin/src/test/projects/ExpediterPluginIntegrationTest/android lib agp9/settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "test-android-library"
